a0d046212c42caed96293e5885d3b5b9efbc5fba
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-01 22:35:23 +0200

    reset.sh: remove services properly

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-eval $(docker-machine env -u)
+eval $(docker-machine env manager)
+#stop services
+docker service rm $(docker service ls --filter name=master-ms1 -q)
+docker service rm $(docker service ls --filter name=master-ms2 -q)
+#remove machines
 docker-machine rm manager node1 node2 -y
+#unset docker machines env vars
+eval $(docker-machine env -u)
