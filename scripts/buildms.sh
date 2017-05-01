e991f29900512a959da9ef5b6faf2518d1479591
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 00:32:07 +0200

    buildms: add basedir

--- scripts/buildms.sh
+++ scripts/buildms.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+BASEDIR=$(dirname "$0")
 #since we don't have a docker repository, we need to build the image on each node
 eval $(docker-machine env node1)
-docker build -t ms1:master ../ms1
-docker build -t ms2:master ../ms2
+docker build -t ms1:master $BASEDIR/../ms1
+docker build -t ms2:master $BASEDIR/../ms2
 
 eval $(docker-machine env node2)
-docker build -t ms1:master ../ms1
-docker build -t ms2:master ../ms2
+docker build -t ms1:master $BASEDIR/../ms1
+docker build -t ms2:master $BASEDIR/../ms2
 
 #check if everything is ok
 docker-machine ssh node1 "docker images"
