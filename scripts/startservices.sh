53bc139f2524a2ae33d5eddbc104056678895af3
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-01 22:38:49 +0200

    starting ms1 and ms2 with replicated mode

--- scripts/startservices.sh
+++ scripts/startservices.sh
@@ -2,15 +2,16 @@
 
 #start ms1 service
 docker service create \
-    --name master-ms1 \
-      --mode
+  --name master-ms1 \
+  --mode replicated \
   --constraint=node.role!=manager \
-      --publish 3001:3000 \
-        ms1:master
+  --publish 3001:3000 \
+ ms1:master
 
   #start ms2 service
 docker service create \
-    --name=master-ms2  \
-      --constraint=node.role!=manager \
-        --publish 3002:3000 \
-          ms2:master
+  --name=master-ms2  \
+  --mode replicated \
+  --constraint=node.role!=manager \
+  --publish 3002:3000 \
+ ms2:master
