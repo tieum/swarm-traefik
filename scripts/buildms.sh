9ec50f4d81c0665c0311b2fc8e6beacb46ee2620
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 00:16:04 +0200

    build image explicitely on the 2 worker nodes (we don't have a registry)

--- scripts/buildms.sh
+++ scripts/buildms.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#since we don't have a docker repository, we need to build the image on each node
+eval $(docker-machine env node1)
+docker build -t ms1:master ../ms1
+docker build -t ms2:master ../ms2
+
+eval $(docker-machine env node2)
 docker build -t ms1:master ../ms1
 docker build -t ms2:master ../ms2
