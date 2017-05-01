85d2d6b5b1257c94353e96769a45de084b9550c8
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 00:28:23 +0200

    buildms: add logs

--- scripts/buildms.sh
+++ scripts/buildms.sh
@@ -8,3 +8,7 @@ docker build -t ms2:master ../ms2
 eval $(docker-machine env node2)
 docker build -t ms1:master ../ms1
 docker build -t ms2:master ../ms2
+
+#check if everything is ok
+docker-machine ssh node1 "docker images"
+docker-machine ssh node2 "docker images"
