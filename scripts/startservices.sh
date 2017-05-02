8c09a42ef51c15da6ef70c1e30983cc5f636719d
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 04:21:13 +0200

    add service port: 3001 for ms1, 3002 for ms2

--- scripts/startservices.sh
+++ scripts/startservices.sh
@@ -3,17 +3,25 @@
 #start ms1 service
 docker-machine ssh manager "docker service create \
   --name master-ms1 \
-  --label traefik.port=3000 \
+  --label traefik.port=3001 \
   --mode replicated \
   --network testnetwork \
   --constraint=node.role==worker \
+  --publish 3001:4567 \
  ms1:master"
+
+#display ms1 infos
+docker service inspect master-ms1 --pretty
 
   #start ms2 service
 docker-machine ssh manager "docker service create \
   --name=master-ms2  \
-  --label traefik.port=3000 \
+  --label traefik.port=3002 \
   --mode replicated \
   --network testnetwork \
   --constraint=node.role==worker \
+  --publish 3002:4567 \
  ms2:master"
+
+#display ms2 infos
+docker service inspect master-ms2 --pretty
