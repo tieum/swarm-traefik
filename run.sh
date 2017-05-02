bbae6c8a4b061dfb15285d011f77a8e4b36d82a2
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 04:24:15 +0200

    call reset.sh script at the beginning of run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+./scripts/reset.sh
 ./scripts/initcluster.sh
 ./scripts/buildms.sh
 ./scripts/startservices.sh
--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,6 +3,8 @@ eval $(docker-machine env manager)
 #stop services
 docker service rm $(docker service ls --filter name=master-ms1 -q)
 docker service rm $(docker service ls --filter name=master-ms2 -q)
+docker service rm $(docker service ls --filter name=traefik -q)
+docker service rm $(docker service ls --filter name=viz -q)
 #remove machines
 docker-machine rm manager node1 node2 -y
 #unset docker machines env vars
