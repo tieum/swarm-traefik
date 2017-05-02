20a19ea8ef2071d9ea6e19fea5f1e1a2608c246a
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 04:20:22 +0200

    ms2: if we dont want to run an infinite loop, ms2 should not query ms1 again

--- ms2/web.rb
+++ ms2/web.rb
@@ -6,10 +6,5 @@ require 'uri'
 set :bind, '0.0.0.0'
 
 get '/' do
-  uri = URI(ENV['MS_URL'])
-  http = Net::HTTP.new(uri.host, uri.port)
-  response = http.request(Net::HTTP::Get.new(uri.request_uri))
-  status = response.code == '200' ? 'OK': 'NOK'
-
-  "Hello, world. I'm #{status}(#{response.code})\n"
+  "Hello there\n"
 end
