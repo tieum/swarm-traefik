3c8f26e3b605466b4c7328373d15ae6bf7d1e7c2
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 15:04:11 +0200

    stop curl'ing ms1 & ms2 at start, with a local registry it takes up to 1 or 2 minutes

--- init.sh
+++ init.sh
@@ -12,11 +12,7 @@ echo "                          Done!                                      "
 echo "                                                                     "
 echo "  - visualiser: http://$manager_ip:8888                              "
 echo "  - traefik:  : http://$manager_ip:8080                              "
-#give some time for ms1 and ms2 to start
-sleep 10
 echo "  - MS1:      : curl -H Host:master-ms1.example.com $manager_ip      "
-echo "     answer is --> $(curl -s -H Host:master-ms1.example.com $manager_ip) "
 echo "  - MS2:      : curl -H Host:master-ms2.example.com $manager_ip      "
-echo "     answer is --> $(curl -s -H Host:master-ms2.example.com $manager_ip) "
 echo "                                                                     "
 echo "#####################################################################"
