629a2784dbb03ee7f0e174860d51ea2ffbe8617a
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 04:38:59 +0200

    run.sh: add output when done with the available generated endpoints

--- run.sh
+++ run.sh
@@ -3,3 +3,17 @@
 ./scripts/initcluster.sh
 ./scripts/buildms.sh
 ./scripts/startservices.sh
+
+#get swarm manager ip
+manager_ip=$(docker-machine ip manager)
+
+echo "#################################################"
+echo "                                                 "
+echo "                    Done!                        "
+echo "                                                 "
+echo "  - visualiser: http://$manager_ip:8888          "
+echo "  - traefik:  : http://$manager_ip:8080          "
+echo "  - MS1:      : http://$manager_ip:3001          "
+echo "  - MS2:      : http://$manager_ip:3002          "
+echo "                                                 "
+echo "#################################################"
